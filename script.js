6230bad392d61f71283896003a85fc4193b8dd98
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-01 14:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	const board = document.getElementById("board");
+	for(let i=0;i<800;i++){
+		const box=document.createElement("div");
+		box.classList.add("square");
+
+		box.addEventListener("mouseenter",()=>{
+			box.classList.add("hovered");
+			setTimeout(()=>{
+				box.classList.remove("hovered");
+			},1000);
+		});
+		board.appendChild(box);
+	}
+})
